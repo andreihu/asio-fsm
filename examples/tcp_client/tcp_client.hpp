80f1f7cf4cc92570a1b7562cceb61e98b186c2b2
Andras Szabo <andras.szabo@iop-ventures.com>
2019-06-08 23:22:46 +0200

    separated headers to logical units

--- examples/tcp_client/tcp_client.hpp
+++ examples/tcp_client/tcp_client.hpp
@@ -1,14 +1,11 @@
 #pragma once
 
-#include "fsm.hpp"
+#include "events.hpp"
+#include "states.hpp"
+
+#include <fsm.hpp>
 
 #include <asio.hpp>
-
-#include <optional>
-#include <variant>
-#include <system_error>
-#include <functional>
-#include <cmath>
 
 struct context {
     context(asio::io_service& io, const std::string& host, const std::string& service) : io(io), host(host), service(service), nbackoff(0) {}
@@ -18,164 +15,11 @@ struct context {
     size_t                  nbackoff;
 };
 
-// events
-
-class retry {};
-class terminated {
-public:
-    terminated() = default;
-    terminated(const std::error_code& ec) noexcept: ec(ec) {}
-
-    operator std::error_code() const {
-        return ec;
-    }
-
-    std::error_code ec;
-};
-
-class failed {
-public:
-    failed() = default;
-    failed(const std::error_code& ec) noexcept: ec(ec) {}
-    operator std::error_code() const {
-        return ec;
-    }    
-    std::error_code ec;
-};
-
-class resolved {
-public:
-    resolved(asio::ip::tcp::endpoint ep) : ep(ep) {}
-    operator asio::ip::tcp::endpoint() const {
-        return ep;
-    }
-    asio::ip::tcp::endpoint ep;
-};
-
-class connected {
-public:
-    connected(asio::ip::tcp::socket& sock) noexcept: sock(sock) {}
-    std::reference_wrapper<asio::ip::tcp::socket> sock;
-
-    operator asio::ip::tcp::socket&() const {
-        return sock.get();
-    }
-};
-
-// states
-
-class resolving : public state<failed, resolved, terminated> {
-public:
-    resolving(asio::io_service& io, const std::string& addr, const std::string& service) :
-        state(io),
-        resolver(io),
-        addr(addr),
-        service(service)
-    {}
-
-    virtual void on_enter() override {
-        resolver.async_resolve(addr, service, track([this](const std::error_code& ec, asio::ip::tcp::resolver::iterator it) {
-            ec ? complete<failed>(ec) : complete<resolved>(*it);
-        }));
-    }
-
-    virtual void cancel() override {
-        complete<terminated>();
-        resolver.cancel();
-    }
-private:
-    asio::ip::tcp::resolver resolver;
-    std::string             addr;
-    std::string             service;
-};
-
 template<typename Event>
 struct state_factory<resolving, Event, context> {
     auto operator()(const Event&, context& ctx) const {
         return std::make_tuple(std::ref(ctx.io), ctx.host, ctx.service);
     }
-};
-
-
-class connecting : public state<failed, connected, terminated> {
-public:
-    connecting(asio::io_service& io, const asio::ip::tcp::endpoint& ep) :
-        state(io),
-        sock(io),
-        ep(ep)
-    {}
-
-    virtual void on_enter() override {
-        sock.async_connect(ep, track([this](const std::error_code& ec) {
-            ec ? complete<failed>(ec) : complete<connected>(sock);
-        }));
-    }
-
-    virtual void cancel() override {
-        complete<terminated>();
-        sock.cancel();
-    }
-private:
-    asio::ip::tcp::socket   sock;
-    asio::ip::tcp::endpoint ep;
-};
-
-class online : public state<failed, terminated> {
-public:
-    online(asio::io_service& io, asio::ip::tcp::socket& sock) :
-        state(io),
-        sock(std::move(sock)),
-        timer(io),
-        extend(false)
-    {}
-
-    virtual void on_enter() override {
-        start_read_socket();
-        start_wait_timer();
-    }
-
-    virtual void cancel() override {
-        complete<terminated>();
-        sock.cancel();
-        timer.cancel();
-    }
-
-private:
-    void start_read_socket() {
-        using namespace std::literals;
-        asio::async_read_until(sock, asio::dynamic_buffer(rx_buffer), "\n"sv, track([this] (const std::error_code& ec, size_t bytes_transferred) {
-            if (ec) {
-                return complete<failed>(ec);
-            }
-
-            log("got %s", rx_buffer);
-            rx_buffer.clear();
-            start_read_socket();
-            extend = true;
-            start_wait_timer();
-        }));
-    }
-
-    void start_wait_timer() {
-        timer.expires_from_now(std::chrono::seconds(10));
-        timer.async_wait(track([this](const std::error_code& ec) {
-            if (ec) {
-                if (ec == asio::error::operation_aborted && extend) {
-                    extend = false;
-                    return;
-                }
-
-                return complete<failed>(ec);
-            }
-
-            return complete<failed>(make_error_code(std::errc::timed_out));
-        }));
-    }
-private:
-    asio::ip::tcp::socket   sock;
-    asio::steady_timer      timer;
-    bool                    extend;
-    std::string             rx_buffer;
 };
 
 template<typename Event>
@@ -186,38 +30,12 @@ struct state_factory<online, Event, context> {
     }
 };
 
-class backoff : public state<retry, failed, terminated> {
-public:
-    backoff(asio::io_service& io, std::chrono::seconds cooldown) :
-        state(io),
-        timer(io)
-    {
-        timer.expires_from_now(cooldown);
-    }
-
-    virtual void on_enter() override {
-        timer.async_wait(track([this](const std::error_code& ec) {
-            ec ? complete<failed>(ec) : complete<retry>();
-        }));
-    }
-
-    virtual void cancel() override {
-        timer.cancel();
-    }
-private:
-    asio::steady_timer timer;
-};
-
 template<typename Event>
 struct state_factory<backoff, Event, context> {
     auto operator()(const Event&, context& ctx) const {
         std::chrono::seconds timeout(std::min<int>(16, std::pow(2, ctx.nbackoff++)));
         return std::make_tuple(std::ref(ctx.io), timeout);
     }
-};
-
-struct completed {
-    completed(asio::io_service& io, const std::error_code& ec) {}
 };
 
 using client = fsm<std::error_code, resolving, completed, context, transitions<
--- include/fsm.hpp
+++ include/fsm.hpp
@@ -1,68 +1,12 @@
+#pragma once
+
 #include <asio.hpp>
 
 #include <variant>
-#include <system_error>
+#include <exception>
 #include <functional>
 #include <type_traits>
 #include "helpers.hpp"
-
-class state_base {
-public:
-    state_base(asio::io_service& io) : io(io) {}
-    virtual void cancel() = 0;
-    virtual ~state_base()  = default;
-protected:
-    asio::io_service&       io;
-}; // class state_base
-
-template<typename ...Events>
-class state : public state_base {
-public:
-    using result = std::variant<Events...>;
-    using completion_handler = std::function<void(const result&)>;
-    state(asio::io_service& io) : state_base(io), rc(0) {}
-
-    void async_wait(completion_handler cb) {
-        if (this->cb) {
-            throw std::runtime_error("state is already active");
-        }
-
-        this->cb = std::move(cb);
-        on_enter();
-    }
-
-    virtual void on_enter() = 0;
-    virtual ~state() override = default;
-
-    template<typename V, typename ...Args>
-    void complete(Args&& ...args) {
-        if (!res) {
-            res.emplace(std::in_place_type<V>, std::forward<Args>(args)...);
-            cancel();
-        }
-    }
-
-    template<class Callable>
-    auto track(Callable&& callable) {
-        ++rc;
-        return [this, callable = std::forward<Callable>(callable)](auto&&... args) -> decltype(auto) {
-            scope_exit _([this] {
-                if (--rc == 0) {
-                    if (cb) {
-                        io.post(std::bind(*cb, *res));
-                        cb = std::nullopt;
-                        res = std::nullopt;
-                    }
-                }
-            });
-            return callable(std::forward<decltype(args)>(args)...);
-        };
-    }
-private:
-    std::optional<completion_handler>       cb;
-    std::optional<result>                   res;
-    size_t                                  rc;
-}; // class state
 
 template<typename State, typename Event, typename Context>
 struct state_factory {
@@ -216,7 +160,7 @@ public:
     template<typename ...Args2>
     void async_wait(completion_handler cb, Args2 ...args) {
         if (sess) {
-            return io.post(std::bind(cb, make_error_code(std::errc::operation_in_progress)));
+            throw std::runtime_error("state machine already active");
         }
 
         sess.emplace(io, std::move(cb), std::forward<Args2>(args)...);
@@ -301,7 +245,6 @@ private:
             using event_type = std::decay_t<decltype(v)>;
             transition_table::template assert_match<State, event_type>();
             using next_state_type = typename transition_table::template next_state<State, event_type>;
-            using namespace boost::core;
             log("%s + %s => %s", type_name<State>(), type_name<event_type>(), type_name<next_state_type>());
             if constexpr (!std::is_same_v<next_state_type, end_state>) {
                 auto& old_state = sess->active_state();
